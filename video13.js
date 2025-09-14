c5b4f96fd02bf2b2870da3f4e2fdc7ee60c18fac
Galym <galymsakitzhan@gmail.com>
2025-09-14 22:36:39 +0500

    first commit

---
+++ video13.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const category = "harry";
+
+console.log(`https://someurl/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
+
+
---
+++ video14.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log(4 + +("5")); // если алдында + туратын болса тогда санга айналып кетеды.
+
+let incr = 10,
+    decr = 10;
+
+
+++preincr; // prefix
+--predecr; // prefix
+
+postincr++; // postfix
+postdecr--; // postfix
+
+console.log(preincr); // result = 11
+console.log(postincr); // result = 10
+
+
+const inst1 = false,
+      inst2 = true;
+
+console.log(inst1 && inst2);
+console.log(inst1 || inst2);
+
+
+
